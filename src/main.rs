66aad95e3a20ec024a5336cb6b50c5629d5e0009
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-17 22:17:18 +0800

    chore: rotating cube

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ const CAT_SIZE: Vec3 = Vec3::from_array([40.0, 40.0, 0.0]);
 #[derive(Debug, Component)]
 struct CenterPlayer;
 
+fn roate(mut query: Query<&mut Transform, With<CenterPlayer>>, timer: Res<Time>) {
+    let mut player_trans = query.single_mut();
+    player_trans.rotate_z(5.0 * timer.delta_seconds());
+}
+
 fn setup(mut commands: Commands, _assert_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
@@ -24,5 +29,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, roate)
         .run();
 }
