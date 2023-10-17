f22a55f51ffb9231d5096b7d3d05a4a0455e4439
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-17 22:07:04 +0800

    chore: add center object

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use bevy::prelude::*;
 
+const CAT_SIZE: Vec3 = Vec3::from_array([40.0, 40.0, 0.0]);
+
+#[derive(Debug, Component)]
+struct CenterPlayer;
+
+fn setup(mut commands: Commands, _assert_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                scale: CAT_SIZE,
+                ..default()
+            },
+            sprite: Sprite { ..default() },
+            ..default()
+        },
+        CenterPlayer,
+    ));
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
