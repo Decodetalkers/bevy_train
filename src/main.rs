d0f93eefebf6ce490472564711ea4b2fa57675e0
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-18 13:56:11 +0800

    chore: collide

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 
 const CAT_SIZE: Vec3 = Vec3::from_array([40.0, 40.0, 0.0]);
 const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+
+const WALL_THICKNESS: f32 = 10.0;
+// x coordinates
+const LEFT_WALL: f32 = -450.;
+const RIGHT_WALL: f32 = 450.;
+// y coordinates
+const BOTTOM_WALL: f32 = -300.;
+const TOP_WALL: f32 = 300.;
+
+const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 
 #[derive(Default, Debug, Resource)]
 struct PlayerState {
@@ -17,6 +30,83 @@ struct SpeedPanel;
 
 #[derive(Debug, Component)]
 struct PosPanel;
+
+#[derive(Event, Default)]
+struct CollisionEvent;
+
+#[derive(Component)]
+struct Collider;
+
+#[derive(Bundle)]
+struct WallBundle {
+    // You can nest bundles inside of other bundles like this
+    // Allowing you to compose their functionality
+    sprite_bundle: SpriteBundle,
+    collider: Collider,
+}
+
+/// Which side of the arena is this wall located on?
+enum WallLocation {
+    Left,
+    Right,
+    Bottom,
+    Top,
+}
+
+impl WallLocation {
+    fn position(&self) -> Vec2 {
+        match self {
+            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
+            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
+            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
+            WallLocation::Top => Vec2::new(0., TOP_WALL),
+        }
+    }
+
+    fn size(&self) -> Vec2 {
+        let arena_height = TOP_WALL - BOTTOM_WALL;
+        let arena_width = RIGHT_WALL - LEFT_WALL;
+        // Make sure we haven't messed up our constants
+        assert!(arena_height > 0.0);
+        assert!(arena_width > 0.0);
+
+        match self {
+            WallLocation::Left | WallLocation::Right => {
+                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
+            }
+            WallLocation::Bottom | WallLocation::Top => {
+                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
+            }
+        }
+    }
+}
+
+impl WallBundle {
+    // This "builder method" allows us to reuse logic across our wall entities,
+    // making our code easier to read and less prone to bugs when we change the logic
+    fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
+                    // This is used to determine the order of our sprites
+                    translation: location.position().extend(0.0),
+                    // The z-scale of 2D objects must always be 1.0,
+                    // or their ordering will be affected in surprising ways.
+                    // See https://github.com/bevyengine/bevy/issues/4149
+                    scale: location.size().extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        }
+    }
+}
 
 fn roate(
     mut query: Query<&mut Transform, With<CenterPlayer>>,
@@ -116,6 +206,11 @@ fn setup(mut commands: Commands, _assert_server: Res<AssetServer>) {
             }),
         )
         .insert(PosPanel);
+
+    commands.spawn(WallBundle::new(WallLocation::Left));
+    commands.spawn(WallBundle::new(WallLocation::Right));
+    commands.spawn(WallBundle::new(WallLocation::Bottom));
+    commands.spawn(WallBundle::new(WallLocation::Top));
 }
 
 fn handle_speedtext(state: Res<PlayerState>, mut query: Query<&mut Text, With<SpeedPanel>>) {
@@ -149,12 +244,36 @@ fn handle_move(mut state: ResMut<PlayerState>, keyboard_input: Res<Input<KeyCode
     }
 }
 
+fn check_collider(
+    mut state: ResMut<PlayerState>,
+    query: Query<&Transform, With<CenterPlayer>>,
+    collider_query: Query<&Transform, With<Collider>>,
+) {
+    let player_trans = query.single().translation;
+    let scale = query.single().scale;
+    for transform in &collider_query {
+        let collision = collide(
+            player_trans,
+            scale.truncate(),
+            transform.translation,
+            transform.scale.truncate(),
+        );
+        if let Some(coll) = collision {
+            match coll {
+                Collision::Left | Collision::Right => state.x = -state.x,
+                Collision::Top | Collision::Bottom => state.y = -state.y,
+                Collision::Inside => { /* do nothing */ }
+            }
+        }
+    }
+}
+
 fn main() {
     App::new()
         .insert_resource(PlayerState::default())
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, (handle_speedtext, handle_postext, roate))
-        .add_systems(Update, handle_move)
+        .add_systems(Update, (check_collider, handle_move.after(check_collider)))
         .run();
 }
