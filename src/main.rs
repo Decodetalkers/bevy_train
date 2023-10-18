0b427c4b2d3f0f5de7332747f11e3bb6ff13022a
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-18 09:57:48 +0800

    chore: add move handle

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,25 @@ use bevy::prelude::*;
 
 const CAT_SIZE: Vec3 = Vec3::from_array([40.0, 40.0, 0.0]);
 
+#[derive(Default, Debug, Resource)]
+struct PlayerState {
+    x: f32,
+    y: f32,
+}
+
 #[derive(Debug, Component)]
 struct CenterPlayer;
 
-fn roate(mut query: Query<&mut Transform, With<CenterPlayer>>, timer: Res<Time>) {
+fn roate(
+    mut query: Query<&mut Transform, With<CenterPlayer>>,
+    timer: Res<Time>,
+    state: Res<PlayerState>,
+) {
     let mut player_trans = query.single_mut();
     player_trans.rotate_z(5.0 * timer.delta_seconds());
+
+    player_trans.translation.x += state.x * timer.delta_seconds();
+    player_trans.translation.y += state.y * timer.delta_seconds();
 }
 
 fn setup(mut commands: Commands, _assert_server: Res<AssetServer>) {
@@ -25,10 +38,27 @@ fn setup(mut commands: Commands, _assert_server: Res<AssetServer>) {
     ));
 }
 
+fn handle_move(mut state: ResMut<PlayerState>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.just_pressed(KeyCode::Left) {
+        state.x -= 50.0;
+    }
+    if keyboard_input.just_pressed(KeyCode::Right) {
+        state.x += 50.0;
+    }
+    if keyboard_input.just_pressed(KeyCode::Down) {
+        state.y -= 50.0;
+    }
+    if keyboard_input.just_pressed(KeyCode::Up) {
+        state.y += 50.0;
+    }
+}
+
 fn main() {
     App::new()
+        .insert_resource(PlayerState::default())
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, roate)
+        .add_systems(Update, handle_move)
         .run();
 }
